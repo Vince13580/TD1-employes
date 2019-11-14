de00e8f4102442ab53b7f27d7cebe23f67d7eea9
t18003855 <thouvenel.vincent@gmail.com>
2019-11-14 17:26:58 +0100

    commit 3

--- src/main/java/fr/univ_amu/iut/exo2/GestionEntreprises.java
+++ src/main/java/fr/univ_amu/iut/exo2/GestionEntreprises.java
@@ -1,7 +1,20 @@
 package fr.univ_amu.iut.exo2;
 
+import fr.univ_amu.iut.exo1.Employe;
+import fr.univ_amu.iut.exo1.Entreprise;
+
+import java.time.LocalDate;
+import java.time.Month;
+
 public class GestionEntreprises {
     public static void main(String[] args) {
-        throw new RuntimeException("Not yet implemented !");
+        Entreprise IUT = new Entreprise("IUT");
+        Employe Maxime = new Employe(0, 120 , "Maxime","Laforce", 1 , LocalDate.of(2000, Month.NOVEMBER,5),LocalDate.of(2019, Month.NOVEMBER,14),20.0,80.0);
+        Employe Matteo = new Employe(1, 121 , "Matteo","Cosson", 1 , LocalDate.of(2000, Month.NOVEMBER,5),LocalDate.of(2019, Month.NOVEMBER,14),20.0,80.0);
+        IUT.hire(Maxime);
+        IUT.hire(Matteo);
+        System.out.println(IUT.toString());
+
+        //throw new RuntimeException("Not yet implemented !");
     }
 }
